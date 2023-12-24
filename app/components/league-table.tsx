5b257852622fb0b90cd74d2e11262ca426dfe12c
Cleberson Junior <cleberson.dev@gmail.com>
2023-12-24 01:57:40 -0300

    Refactor League table

--- app/components/league-table.tsx
+++ app/components/league-table.tsx
@@ -22,6 +22,45 @@ const tableHeaders = [
   { key: "form", label: "Form" },
 ];
 
+const mapTeamToRowData = (tableTeam: any, idx: number, league: League): {
+  key: string;
+  element?: any;
+  value?: any;
+  showHorizontalPadding?: boolean;
+  align?: 'left' | 'center' | 'right';
+  fullWidth?: boolean;
+  bold?: boolean;
+}[] => [
+  { key: "position", value: idx + 1, showHorizontalPadding: true, },
+  { key: "team", value: league.teams[tableTeam.team].name, align: 'left', fullWidth: true, showHorizontalPadding: true, },
+  { key: "points", value: tableTeam.points, bold: true },
+  { key: "played", value: tableTeam.games },
+  { key: "wins", value: tableTeam.wins },
+  { key: "draws", value: tableTeam.draws },
+  { key: "losses", value: tableTeam.losses },
+  { key: "goalsFor", value: tableTeam.goalsScored, showHorizontalPadding: true, },
+  { key: "goalsAgainst", value: tableTeam.goalsConceived, showHorizontalPadding: true, },
+  { key: "goalsDifference", value: tableTeam.goalsDifference, showHorizontalPadding: true, },
+  { 
+    key: "form", 
+    showHorizontalPadding: true,
+    element: (
+      <div className="flex gap-x-1">
+        {tableTeam.results.slice(0, 5).reverse().map((result: any) => (
+          <div className={
+            cls({
+              "h-2 w-2 rounded-full": true,
+              "bg-slate-300": result === "DRAW",
+              "bg-green-400": result === "WIN",
+              "bg-red-400": result === "LOSS",
+            })
+          }></div>
+        ))}
+      </div>
+    )
+  },
+] 
+
 export default function LeagueTable({ league }: Props) {
   return (
     <table className="w-full table-auto shadow">
@@ -31,10 +70,10 @@ export default function LeagueTable({ league }: Props) {
             <th 
               key={header.key}
               className={cls({
-                "py-4 font-black": true,
+                "py-4 font-black first:pl-6 last:pr-6": true,
                 "px-4": !header.hideHorizontalPadding,
-                "rounded-tl-2xl pl-6": idx === 0,
-                "rounded-tr-2xl pr-6": idx === tableHeaders.length - 1,
+                "rounded-tl-2xl": idx === 0,
+                "rounded-tr-2xl": idx === tableHeaders.length - 1,
                 "text-left": header.align === 'left',
               })} 
             >
@@ -53,30 +92,20 @@ export default function LeagueTable({ league }: Props) {
             })}
             key={league.teams[team.team].name}
           >
-            <td className="px-4 py-4 pl-6">{idx + 1}</td>
-            <td className="w-full px-4 py-4 text-left">
-              {league.teams[team.team].name}
-            </td>
-            <td className="py-4 font-bold">{team.points}</td>
-            <td className="py-4">{team.games}</td>
-            <td className="py-4">{team.wins}</td>
-            <td className="py-4">{team.draws}</td>
-            <td className="py-4">{team.losses}</td>
-            <td className="px-4 py-4">{team.goalsScored}</td>
-            <td className="px-4 py-4">{team.goalsConceived}</td>
-            <td className="px-4 py-4">{team.goalsDifference}</td>
-            <td className="flex gap-x-1 px-4 py-6 pr-6">
-              {team.results.slice(0, 5).reverse().map(result => (
-                <div className={
-                  cls({
-                    "h-2 w-2 rounded-full": true,
-                    "bg-slate-300": result === "DRAW",
-                    "bg-green-400": result === "WIN",
-                    "bg-red-400": result === "LOSS",
-                  })
-                }></div>
-              ))}
-            </td>
+            {mapTeamToRowData(team, idx, league).map(colData => (
+              <td
+                key={colData.key}
+                className={cls({
+                  "py-4 first:pl-6 last:pr-6": true,
+                  "px-4": colData.showHorizontalPadding,
+                  "font-bold": colData.bold,
+                  "w-full": colData.fullWidth,
+                  "text-left": colData.align === 'left',
+                })}
+              >
+                {colData.element ?? colData.value}
+              </td>
+            ))}
           </tr>
         ))}
       </tbody>
