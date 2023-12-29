cc0dd3507d4525aa59b58fb0efdb5ada165a9fd6
Cleberson Junior <cleberson.dev@gmail.com>
2023-12-28 22:48:06 -0300

    Add validation to create league form

--- app/components/create-league.modal.tsx
+++ app/components/create-league.modal.tsx
@@ -1,19 +1,30 @@
 import CreatableSelect from "react-select/creatable";
 import Button from "./button";
 import { Team } from "~/entities/League";
+import { useForm, Form, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type Props = {
   teams: Team[];
   onClose: () => void;
 };
 
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    teams: yup.array().of(yup.string()).min(4).required(),
+  })
+  .required();
+
 export default function CreateLeagueModal({ teams, onClose }: Props) {
+  const { control, register, formState: { errors, isValid }, setValue } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   return (
     <div className="z-20 absolute left-0 top-0 flex h-[100svh] w-full items-center justify-center bg-black/10">
-      <form
-        method="POST"
-        className="flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow"
-      >
+      <Form className="flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow" control={control}>
         <input
           readOnly
           name="actionType"
@@ -26,38 +37,49 @@ export default function CreateLeagueModal({ teams, onClose }: Props) {
           <div>
             <label className="block">Name</label>
             <input
-              required
-              name="name"
               className="w-60 rounded border border-solid border-black/10"
+              {...register("name")}
             />
+            {errors.name && <span className="text-red-500">{errors.name.message}</span>}
           </div>
           <div>
             <label className="block">Teams</label>
             <div className="mb-2 flex flex-col gap-y-2">
-              <CreatableSelect
-                isMulti
+              <Controller
                 name="teams"
-                options={teams.map((team) => ({
-                  label: team.name,
-                  value: JSON.stringify({ id: team.id }),
-                }))}
-                getNewOptionData={(newOption) => {
-                  return {
-                    label: newOption,
-                    value: JSON.stringify({ name: newOption }),
-                  };
-                }}
+                control={control}
+                render={({ field }) => (
+                  <CreatableSelect
+                    isMulti
+                    ref={field.ref}
+                    name={field.name}
+                    onBlur={field.onBlur}
+                    onChange={newValues => field.onChange(newValues.map(({ value }) => value))}
+                    options={teams.map((team) => ({
+                      label: team.name,
+                      value: JSON.stringify({ id: team.id }),
+                    }))}
+                    getNewOptionData={(newOption) => {
+                      return {
+                        label: newOption,
+                        value: JSON.stringify({ name: newOption }),
+                      };
+                    }}
+                  />
+                )}
               />
+             
+              {errors.teams && <span className="text-red-500">{errors.teams.message}</span>}
             </div>
           </div>
         </div>
         <div className="flex justify-end gap-x-2">
-          <Button type="submit">Create</Button>
+          <Button type="submit" disabled={!isValid}>Create</Button>
           <Button type="button" onClick={onClose}>
             Close
           </Button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
