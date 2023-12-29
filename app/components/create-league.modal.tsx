e43b7bfcc0e99d154e830703aa51dfdd064d1c86
Cleberson Junior <cleberson.dev@gmail.com>
2023-12-29 12:14:36 -0300

    Fix converting hook form data to regular form data

--- app/components/create-league.modal.tsx
+++ app/components/create-league.modal.tsx
@@ -1,9 +1,12 @@
 import CreatableSelect from "react-select/creatable";
-import Button from "./button";
-import { Team } from "~/entities/League";
-import { useForm, Form, Controller } from "react-hook-form";
+import { useFetcher } from "@remix-run/react";
+import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+
+import { Team } from "~/entities/League";
+
+import Button from "~/components/button";
 
 type Props = {
   teams: Team[];
@@ -13,25 +16,39 @@ type Props = {
 const schema = yup
   .object({
     name: yup.string().required(),
-    teams: yup.array().of(yup.string()).min(4).required(),
+    teams: yup.array().of(yup.string()).min(4).required()
+      .test("evenTeamsCount", "The number of teams should be even", (teams => teams.length % 2 === 0)),
   })
   .required();
 
 export default function CreateLeagueModal({ teams, onClose }: Props) {
-  const { control, register, formState: { errors, isValid }, setValue } = useForm({
+  const fetcher = useFetcher();
+  
+  const { control, register, formState: { errors }, watch, handleSubmit } = useForm({
     resolver: yupResolver(schema),
+    values: {
+      name: "",
+      teams: [],
+    },
   });
+  const teamsCount = (watch("teams") || []).length;
+
+  const onSubmit = (values: any) => {
+    const formData = new FormData();
+    formData.append("actionType", "createLeague");
+    Object.entries(values).forEach(([key, val]) => {
+      if (Array.isArray(val)) {
+        val.forEach(team => formData.append(key, team));
+        return;
+      }
+      formData.append(key, val as any);
+    });
+    fetcher.submit(formData, { action: "/dashboard", method: "POST" });
+  }
 
   return (
     <div className="z-20 absolute left-0 top-0 flex h-[100svh] w-full items-center justify-center bg-black/10">
-      <Form className="flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow" control={control}>
-        <input
-          readOnly
-          name="actionType"
-          value="createLeague"
-          className="hidden"
-        />
-
+      <form className="flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow" onSubmit={handleSubmit(onSubmit)}>
         <h1 className="mb-10 text-2xl font-bold">Create your league</h1>
         <div className="flex flex-grow flex-col gap-y-8 overflow-auto">
           <div>
@@ -43,7 +60,7 @@ export default function CreateLeagueModal({ teams, onClose }: Props) {
             {errors.name && <span className="text-red-500">{errors.name.message}</span>}
           </div>
           <div>
-            <label className="block">Teams</label>
+            <label className="block">Teams ({teamsCount} added)</label>
             <div className="mb-2 flex flex-col gap-y-2">
               <Controller
                 name="teams"
@@ -74,12 +91,12 @@ export default function CreateLeagueModal({ teams, onClose }: Props) {
           </div>
         </div>
         <div className="flex justify-end gap-x-2">
-          <Button type="submit" disabled={!isValid}>Create</Button>
+          <Button type="submit">Create</Button>
           <Button type="button" onClick={onClose}>
             Close
           </Button>
         </div>
-      </Form>
+      </form>
     </div>
   );
 }
