2304518690c33b5b293817fa6aacd0a61b0376d7
Cleberson Junior <cleberson.dev@gmail.com>
2023-12-29 17:05:25 -0300

    Fix login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,40 +1,75 @@
-import { ActionFunction } from '@remix-run/server-runtime'
-import { Form, useActionData, Link } from "@remix-run/react";
-import { badRequest } from 'remix-utils';
-import { createUserSession, login } from '~/utils/session.server';
+import { Link } from "@remix-run/react";
+import { badRequest } from "remix-utils";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-export default function Register() {
-  const actionData = useActionData<typeof action>();
+import Button from "~/components/button";
+import { createUserSession, login } from "~/utils/session.server";
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(16).required(),
+});
+
+export default function LoginPage() {
+  const {
+    register,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+  });
 
   return (
-    <Form className="flex flex-col gap-y-4 justify-center items-center min-h-[100svh]" method="POST">
-      <h1 className="font-bold text-2xl">Login</h1>
-      <input placeholder="Enter your e-mail address" type="email" name="email" required />
-      <input placeholder="Enter your password" type="password" name="password" required minLength={8} maxLength={16} />
-      <button className="bg-primary-dark py-2 px-3 rounded" type="submit">Login</button>
-      <p className="text-xs text-red-400">{actionData?.error}</p>
-      <Link to="/register" className='underline text-blue-500'>Register</Link>
-    </Form>
+    <form
+      className="flex min-h-[100svh] flex-col items-center justify-center gap-y-4"
+      method="POST"
+    >
+      <h1 className="text-2xl font-bold">Login</h1>
+
+      <div>
+        <label className="block">E-mail</label>
+        <input type="email" {...register("email")} />
+        {errors.email && (
+          <p className="text-red-500">
+            <small>{errors.email.message}</small>
+          </p>
+        )}
+      </div>
+      <div>
+        <label className="block">Password</label>
+        <input type="password" {...register("password")} />
+        {errors.password && (
+          <p className="text-red-500">
+            <small>{errors.password.message}</small>
+          </p>
+        )}
+      </div>
+      <Button type="submit" disabled={!isValid}>
+        Login
+      </Button>
+
+      <Link to="/register" className="text-blue-500 underline">
+        Register
+      </Link>
+    </form>
   );
 }
 
-export const action: ActionFunction = async ({
-  request,
-}: any) => {
+export const action = async ({ request }: any) => {
   const form = await request.formData();
 
-  const data = {
-    email: form.get("email"),
-    password: form.get("password"),
-  }
+  const email = form.get("email") as string;
+  const password = form.get("password") as string;
 
-  const user = await login({ email: data.email, password: data.password });
+  const user = await login({ email, password });
 
   if (!user) {
     return badRequest({
-      error: 'Username and/or password are invalid',
+      error: "Username and/or password are invalid",
     });
   }
 
   return createUserSession(user.id, "/dashboard");
-}
+};
