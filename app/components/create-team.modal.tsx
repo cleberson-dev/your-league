0628d86af02d1440ad76aaeee8bac617ac839710
Cleberson Junior <cleberson.dev@gmail.com>
2023-12-29 12:15:09 -0300

    Add hook forms to create team modal

--- app/components/create-team.modal.tsx
+++ app/components/create-team.modal.tsx
@@ -1,16 +1,31 @@
+import { useForm, Form } from "react-hook-form";
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import Button from "./button"
 
 type Props = {
   onClose: () => void;
 }
 
+const schema = yup.object({
+  name: yup.string().min(3).max(32).required(),
+  logo: yup.mixed().test("fileType", "File type must be png, jpeg or svg", (value: any) => {
+    if (!value.length) return true; // logo should be optional
+    return value.type.match(/jpg|jpeg|png|svg/);
+  }),
+})
+
 export default function CreateTeamModal({ onClose }: Props) {
+  const { control, register, formState: { errors, isValid } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   return (
     <div className="z-20 fixed left-0 top-0 flex h-[100svh] w-full items-center justify-center bg-black/10">
-      <form
+      <Form
         encType="multipart/form-data"
         className="flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow"
-        method="POST"
+        control={control}
       >
         <input readOnly name="actionType" value="createTeam" className="hidden" />
 
@@ -19,28 +34,29 @@ export default function CreateTeamModal({ onClose }: Props) {
           <div>
             <label className="block">Name</label>
             <input
-              required
-              name="name"
               className="w-60 rounded border border-solid border-black/10"
               autoComplete="off"
+              {...register("name")}
             />
           </div>
+          {errors.name && <span className="text-red-500 text-xs">{errors.name.message}</span>}
           <div>
             <label className="block">Logo</label>
             <input
               type="file"
-              name="logo"
               accept=".jpeg, .png, .svg, .jpg"
+              {...register("logo")}
             />
+            {errors.logo && <span className="text-red-500 text-xs">{errors.logo.message}</span>}
           </div>
         </div>
         <div className="flex justify-end gap-x-2">
-          <Button type="submit">Create</Button>
+          <Button type="submit" disabled={!isValid}>Create</Button>
           <Button type="button" onClick={onClose}>
             Close
           </Button>
         </div>
-      </form>
+      </Form>
     </div>
   )
 }
