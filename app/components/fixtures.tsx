348df07556584f5a4cc22f571167c1bd9c65c130
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-06 21:00:03 -0300

    Allow simulation of games one by one

--- app/components/fixtures.tsx
+++ app/components/fixtures.tsx
@@ -4,57 +4,101 @@ import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/16/solid";
 import cls from "classnames";
 import { Fixtures, Team } from "~/entities/League.entity";
+import { useFormContext } from "react-hook-form";
 
 type Props = {
   fixtures: Fixtures;
   teams: Team[];
-  onTeamClicked?: (roundIdx: number, gameIdx: number, homeOrAway: "home" | "away") => void;
-}
+  onTeamClicked?: (
+    roundIdx: number,
+    gameIdx: number,
+    homeOrAway: "home" | "away"
+  ) => void;
+  inSimulation: boolean;
+};
 
-export default function Fixtures({ fixtures, teams, onTeamClicked }: Props) {
+export default function Fixtures({
+  fixtures,
+  teams,
+  onTeamClicked,
+  inSimulation,
+}: Props) {
   const [currentRound, setCurrentRound] = useState(0);
 
   const goPrevRound = () => setCurrentRound(currentRound - 1);
   const goNextRound = () => setCurrentRound(currentRound + 1);
 
+  const { register } = useFormContext();
+
+  const scoreInputClassnames =
+    "w-16 text-black [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none";
+
   return (
-    <div className="text-center bg-primary dark:bg-dark/50 shadow self-start">
-      <div className="flex items-center bg-primary-dark dark:bg-dark text-black/50 dark:text-white/50 p-2 rounded-t select-none">
-        <button className="disabled:opacity-10" onClick={goPrevRound} disabled={currentRound === 0}>
+    <div className="self-start bg-primary text-center shadow dark:bg-dark/50">
+      <div className="flex select-none items-center rounded-t bg-primary-dark p-2 text-black/50 dark:bg-dark dark:text-white/50">
+        <button
+          className="disabled:opacity-10"
+          onClick={goPrevRound}
+          disabled={currentRound === 0}
+        >
           <ChevronLeftIcon width={16} height={16} />
         </button>
-        <h1 className="font-black flex-grow text-sm lowercase">Round {currentRound + 1}</h1>
-        <button className="disabled:opacity-10" onClick={goNextRound} disabled={currentRound === fixtures.length - 1}>
+        <h1 className="flex-grow text-sm font-black lowercase">
+          Round {currentRound + 1}
+        </h1>
+        <button
+          className="disabled:opacity-10"
+          onClick={goNextRound}
+          disabled={currentRound === fixtures.length - 1}
+        >
           <ChevronRightIcon width={16} height={16} />
         </button>
       </div>
 
       <ul className="text-sm">
         {fixtures[currentRound].map((game, gameIdx) => (
-          <li key={gameIdx} className={cls({
-            "grid grid-cols-[1fr_3rem_1fr] items-center hover:bg-primary-dark/30 dark:hover:bg-dark transition-colors": true,
-            "bg-slate-100 dark:bg-dark/50": game.finished,
-          })}>
-            <span 
+          <li
+            key={gameIdx}
+            className={cls({
+              "grid grid-cols-[1fr_3rem_1fr] items-center transition-colors hover:bg-primary-dark/30 dark:hover:bg-dark":
+                true,
+              "bg-slate-100 dark:bg-dark/50": game.finished,
+            })}
+          >
+            <span
               className={cls({
-                "p-2 cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap": true,
+                "cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap p-2":
+                  true,
                 "bg-green/10 font-medium": game.homeScore! > game.awayScore!,
                 "bg-red/10": game.awayScore! > game.homeScore!,
-              })} 
-              onClick={() => onTeamClicked?.(currentRound, gameIdx, "home")}
+              })}
+              onClick={() =>
+                !inSimulation && onTeamClicked?.(currentRound, gameIdx, "home")
+              }
             >
               {teams[game.homeTeam!].name}
             </span>
             <span>
-              {game.finished ? `${game.homeScore} x ${game.awayScore}` : "x"}
-            </span> 
-            <span 
+              {inSimulation && (
+                <div className="flex items-center">
+                  <input type="number" className={scoreInputClassnames} {...register(`fixtures.${currentRound}.${gameIdx}.home`)} /> x{" "}
+                  <input type="number" className={scoreInputClassnames} {...register(`fixtures.${currentRound}.${gameIdx}.away`)} />
+                </div>
+              )}
+              {!inSimulation && game.finished
+                ? `${game.homeScore} x ${game.awayScore}`
+                : "x"}
+            </span>
+            <span
               className={cls({
-                "p-2 cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap": true,
+                "cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap p-2":
+                  true,
                 "bg-green/10 font-medium": game.awayScore! > game.homeScore!,
                 "bg-red/10": game.homeScore! > game.awayScore!,
-              })} 
-              onClick={() => onTeamClicked?.(currentRound, gameIdx, "away")}
+              })}
+              onClick={() =>
+                !inSimulation && onTeamClicked?.(currentRound, gameIdx, "away")
+              }
             >
               {teams[game.awayTeam!].name}
             </span>
--- app/routes/leagues.$id.tsx
+++ app/routes/leagues.$id.tsx
@@ -9,12 +9,29 @@ import { db } from "~/utils/db.server";
 import { requireUserId } from "~/utils/session.server";
 
 import League from "~/entities/League.entity";
+import { FormProvider, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const gameSchema = yup.object({
+  home: yup.number().min(0),
+  away: yup.number().min(0),
+});
+const roundSchema = yup.array().of(gameSchema);
+const fixturesSchema = yup.array().of(roundSchema);
+const schema = yup.object({ fixtures: fixturesSchema });
 
 export const meta: MetaFunction = ({ data }) => ({
   title: `${data.league.name} | Your League`,
 });
 
-export const loader = async ({ request, params }: { request: Request, params: { id: string; } }) => {
+export const loader = async ({
+  request,
+  params,
+}: {
+  request: Request;
+  params: { id: string };
+}) => {
   const userId = await requireUserId(request);
   try {
     const league = await db.league.findFirstOrThrow({
@@ -31,42 +48,104 @@ export const loader = async ({ request, params }: { request: Request, params: { 
 
 export default function LeaguePage() {
   const loaderData = useLoaderData();
-  const league = new League(loaderData.league.name, loaderData.league.teams, loaderData.league.fixtures);
+  const league = new League(
+    loaderData.league.name,
+    loaderData.league.teams,
+    loaderData.league.fixtures
+  );
 
-  const [simulatedFixtures, setSimulatedFixtures] = useState([...league.fixtures]);
-
-  const simulate = () => {
-    setSimulatedFixtures(simulatedFixtures.map(round => round.map((game) => ({
-      ...game,
-      homeScore: Math.round(Math.random() * 3),
-      awayScore: Math.round(Math.random() * 3),
-      finished: true,
-    }))));
+  const simulateAll = () => {
+    // setSimulatedFixtures(
+    //   simulatedFixtures.map((round) =>
+    //     round.map((game) => ({
+    //       ...game,
+    //       homeScore: Math.round(Math.random() * 3),
+    //       awayScore: Math.round(Math.random() * 3),
+    //       finished: true,
+    //     }))
+    //   )
+    // );
   };
 
-  const simulateGame = (roundIdx: number, gameIdx: number, homeScore: number, awayScore: number) => {
-    setSimulatedFixtures(
-      simulatedFixtures.map((oldRound, oldRoundIdx) => oldRoundIdx === roundIdx ? oldRound.map((oldGame, oldGameIdx) => gameIdx === oldGameIdx ? ({...oldGame, homeScore, awayScore, finished: true}) : oldGame) : oldRound)
-    );
+  // const simulateGame = (
+  // roundIdx: number,
+  // gameIdx: number,
+  // homeScore: number,
+  // awayScore: number
+  // ) => {
+  // setSimulatedFixtures(
+  //   simulatedFixtures.map((oldRound, oldRoundIdx) =>
+  //     oldRoundIdx === roundIdx
+  //       ? oldRound.map((oldGame, oldGameIdx) => gameIdx === oldGameIdx ? { ...oldGame, homeScore, awayScore, finished: true } : oldGame)
+  //       : oldRound
+  //   )
+  // );
+  // };
+
+  const methods = useForm({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+    defaultValues: {
+      fixtures: league.fixtures.map((round) =>
+        round.map(() => ({ home: undefined, away: undefined }))
+      ),
+    },
+  });
+  const formFixtures = methods.watch("fixtures");
+  const simulatedFixtures = league.fixtures.map((round, roundIdx) =>
+    round.map((game, gameIdx) => ({
+      ...game,
+      homeScore: +formFixtures![roundIdx]![gameIdx].home!,
+      awayScore: +formFixtures![roundIdx]![gameIdx].away!,
+      finished:
+        !!formFixtures![roundIdx]![gameIdx].home &&
+        !!formFixtures![roundIdx]![gameIdx].away,
+    }))
+  );
+  const [isInSimulation, setIsInSimulation] = useState(false);
+  const enterInSimulationMode = () => {
+    setIsInSimulation(true);
+    console.log({ values: methods.getValues().fixtures });
   };
 
   return (
     <div className="relative pl-10">
       <div className="p-8">
-        <div className="flex justify-between items-center mb-8">
-          <h1 className="font-bold text-3xl">{league.name}</h1>
-          <button className="px-4 py-2 text-sm bg-green hover:opacity-80 transition-opacity text-white dark:text-black rounded" onClick={simulate}>Simulate</button>
+        <div className="mb-8 flex items-center justify-between">
+          <h1 className="text-3xl font-bold">{league.name}</h1>
+          <div className="flex gap-x-2">
+            <button
+              className="rounded bg-green px-4 py-2 text-sm text-white transition-opacity hover:opacity-80 dark:text-black"
+              onClick={simulateAll}
+            >
+              Simulate
+            </button>
+            <button
+              className="rounded bg-blue-500 px-4 py-2 text-sm text-white transition-opacity hover:opacity-80 dark:text-black"
+              onClick={enterInSimulationMode}
+            >
+              Enter in Simulation
+            </button>
+          </div>
         </div>
 
         <div className="grid grid-cols-[85fr_25fr] gap-x-4">
           <LeagueTable fixtures={simulatedFixtures} teams={league.teams} />
-          <Fixtures 
-            fixtures={simulatedFixtures} 
-            teams={league.teams} 
-            onTeamClicked={(roundIdx, gameIdx, homeOrAway) => {
-              simulateGame(roundIdx, gameIdx, homeOrAway === "home" ? 3 : 0, homeOrAway === "away" ? 3 : 0);
-            }}
-          />
+          <FormProvider {...methods}>
+            <Fixtures
+              inSimulation={isInSimulation}
+              fixtures={simulatedFixtures}
+              teams={league.teams}
+              // onTeamClicked={(roundIdx, gameIdx, homeOrAway) => {
+              //   simulateGame(
+              //     roundIdx,
+              //     gameIdx,
+              //     homeOrAway === "home" ? 3 : 0,
+              //     homeOrAway === "away" ? 3 : 0
+              //   );
+              // }}
+            />
+          </FormProvider>
         </div>
       </div>
     </div>
