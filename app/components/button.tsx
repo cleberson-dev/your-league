98999b6d4e9d4edb9a88e084bf160d84993e7163
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-09 11:42:53 -0300

    Create button variants

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,13 +1,26 @@
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
+import cls from "classnames";
 
-export default function Button({ className, ...props }: Props) {
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "standard" | "error" | "info" | "success" | "neutral";
+};
+
+export default function Button({
+  className,
+  variant = "standard",
+  ...props
+}: Props) {
   return (
     <button
       {...props}
-      className={
-        "rounded bg-violet px-4 py-2 text-sm text-white transition-opacity hover:opacity-50 disabled:bg-slate-300 disabled:text-black disabled:opacity-25 " +
-        className
-      }
+      className={cls({
+        ["rounded px-4 py-2 text-sm transition-opacity hover:opacity-50 disabled:opacity-25 " +
+        className]: true,
+        "bg-violet text-white": variant === "standard",
+        "bg-red text-white": variant === "error",
+        "bg-blue-500 text-white": variant === "info",
+        "bg-green text-white": variant === "success",
+        "bg-slate-100 text-black": variant === "neutral"
+      })}
     >
       {props.children}
     </button>
