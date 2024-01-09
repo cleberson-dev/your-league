6ace17c683873c5acdb8db228b0946217f6b0245
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-08 23:40:41 -0300

    Delete 'remove' button from team card component

--- app/components/team-card.tsx
+++ app/components/team-card.tsx
@@ -1,4 +1,3 @@
-import { TrashIcon } from "@heroicons/react/16/solid";
 import TeamLogo from "~/components/team-logo";
 import { Team } from "~/entities/League.entity";
 
@@ -7,16 +6,13 @@ type Props = {
   removable?: boolean;
 };
 
-export default function TeamCard({ team, removable }: Props) {
+export default function TeamCard({ team }: Props) {
   return (
-    <div className="group relative flex h-36 w-40 select-none flex-col justify-end rounded-3xl border border-solid border-black/5 bg-primary dark:bg-dark dark:text-white py-4 text-center font-medium text-black/50">
-      <div className="absolute top-2 right-2">
-        {removable && <button><TrashIcon width={16} height={16} /></button>}
-      </div>
+    <div className="relative flex h-36 w-40 select-none flex-col justify-end rounded-3xl border border-solid border-black/5 bg-primary py-4 text-center font-medium text-black/50 dark:bg-dark dark:text-white">
       <div className="absolute top-0 flex h-full w-full items-center justify-center">
         <TeamLogo team={team} className="h-12" />
       </div>
-      <span className="overflow-hidden text-ellipsis text-nowrap text-sm group-hover:text-black">
+      <span className="overflow-hidden text-ellipsis text-nowrap text-sm">
         {team.name}
       </span>
     </div>
