0777cb5086dc364bc3f6a0f974f778333664e7a2
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-08 23:39:33 -0300

    Ability to add more classes to button component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,10 +1,13 @@
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button(props: Props) {
+export default function Button({ className, ...props }: Props) {
   return (
-    <button 
+    <button
       {...props}
-      className="bg-violet text-white py-2 px-4 text-sm rounded hover:opacity-50 transition-opacity disabled:bg-slate-300 disabled:text-black disabled:opacity-25"
+      className={
+        "rounded bg-violet px-4 py-2 text-sm text-white transition-opacity hover:opacity-50 disabled:bg-slate-300 disabled:text-black disabled:opacity-25 " +
+        className
+      }
     >
       {props.children}
     </button>
