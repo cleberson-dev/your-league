fc838f0891910a499553255b2af4cbdb744c343f
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-09 10:53:46 -0300

    Style confirmation modal properly

--- app/components/confirmation.modal.tsx
+++ app/components/confirmation.modal.tsx
@@ -10,8 +10,8 @@ export default function ConfirmationModal({
   onClose,
 }: ConfirmationModalProps) {
   return (
-    <div className="flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow dark:bg-dark">
-      <h1>Are you sure you want to do this?</h1>
+    <div className="flex flex-col rounded bg-white p-8 shadow dark:bg-dark">
+      <h1 className="whitespace-nowrap text-3xl font-bold mb-8">Are you sure you want to do this?</h1>
       <div className="flex justify-end gap-x-2">
         <Button type={onConfirm ? "button" : "submit"} onClick={onConfirm}>
           Confirm
