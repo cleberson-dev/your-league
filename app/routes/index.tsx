63dbefbf82d4b5490fc1df1e65107e259d6a1441
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-09 10:32:50 -0300

    Redirect to dashboard for logged-in users

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,20 @@
 import { MetaFunction } from "@remix-run/react/dist/routeModules";
+import { redirect } from "@remix-run/server-runtime";
+import { requireUserId } from "~/utils/session.server";
 
 export const meta: MetaFunction = () => ({
   title: "Home | Your League",
 });
 
+export const loader = async ({ request }: { request: Request }) => {
+  await requireUserId(request);
+
+  return redirect("/dashboard");
+};
+
 export default function Index() {
   return (
-    <div className="min-h-[100svh] py-12 px-16">
+    <div className="min-h-[100svh] px-16 py-12">
       <h1>Home Page</h1>
     </div>
   );
