178a96a07a31dc71f585b55a0dae2593c759d327
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-11 02:08:19 -0300

    Disable autocomplete from score input

--- app/components/table-fixture-score-input.tsx
+++ app/components/table-fixture-score-input.tsx
@@ -6,6 +6,7 @@ const TableFixtureScoreInput = forwardRef<HTMLInputElement, Props>(
   ({ className, ...props }, ref) => (
     <input
       type="number"
+      autoComplete="off"
       className={
         "w-6 border-x-0 border-t-0 bg-transparent p-0 text-center [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none " +
         className
