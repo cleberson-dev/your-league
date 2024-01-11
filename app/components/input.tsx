c4e0c07381a11f000ede69a57cfad4d5f575d63c
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-10 21:35:32 -0300

    Refactor Input

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -3,7 +3,11 @@ import { InputHTMLAttributes, forwardRef } from "react";
 type Props = InputHTMLAttributes<HTMLInputElement>;
 
 const Input = forwardRef<HTMLInputElement, Props>((props, ref) => (
-  <input className="rounded text-sm border-slate-200 bg-slate-100 dark:bg-darker/50 dark:border-darker/60" {...props} ref={ref} />
+  <input
+    className="rounded border-slate-200 bg-slate-100 text-sm dark:border-darker/60 dark:bg-darker/50"
+    {...props}
+    ref={ref}
+  />
 ));
 
 Input.displayName = "Input";
