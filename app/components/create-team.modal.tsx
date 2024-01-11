6c828148c09069d2446ea344f8a9448287826900
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-10 21:35:02 -0300

    Refactor Create Team Modal

--- app/components/create-team.modal.tsx
+++ app/components/create-team.modal.tsx
@@ -3,9 +3,19 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import Button from "./button";
 import createTeamSchema from "~/schemas/create-team.schema";
 import Input from "./input";
+import ErrorMessage from "./error-message";
 
 type Props = {
   onClose: () => void;
+};
+
+const className = {
+  form: "flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow dark:bg-dark",
+  title: "mb-8 text-2xl font-bold",
+  formFields: "flex flex-grow flex-col gap-y-8",
+  formGroup: "flex flex-col gap-y-1",
+  label: "block",
+  actionButtons: "flex justify-end gap-x-2",
 };
 
 export default function CreateTeamModal({ onClose }: Props) {
@@ -20,37 +30,29 @@ export default function CreateTeamModal({ onClose }: Props) {
   return (
     <Form
       encType="multipart/form-data"
-      className="flex h-3/4 w-3/4 flex-col rounded bg-white p-8 shadow dark:bg-dark"
+      className={className.form}
       control={control}
     >
       <input readOnly name="actionType" value="createTeam" className="hidden" />
 
-      <h1 className="mb-8 text-2xl font-bold">Create your team</h1>
-      <div className="flex flex-grow flex-col gap-y-8">
-        <div className="flex flex-col gap-y-1">
-          <label className="block">Name</label>
+      <h1 className={className.title}>Create your team</h1>
+      <div className={className.formFields}>
+        <div className={className.formGroup}>
+          <label className={className.label}>Name</label>
           <Input autoComplete="off" {...register("name")} />
-          {errors.name && (
-            <p className="text-red">
-              <small>{errors.name.message}</small>
-            </p>
-          )}
+          <ErrorMessage message={errors.name?.message} />
         </div>
-        <div className="flex flex-col gap-y-1">
-          <label className="block">Logo</label>
+        <div className={className.formGroup}>
+          <label className={className.label}>Logo</label>
           <input
             type="file"
             accept=".jpeg, .png, .svg, .jpg"
             {...register("logo")}
           />
-          {errors.logo && (
-            <p className="text-red">
-              <small>{errors.logo.message}</small>
-            </p>
-          )}
+          <ErrorMessage message={errors.logo?.message} />
         </div>
       </div>
-      <div className="flex justify-end gap-x-2">
+      <div className={className.actionButtons}>
         <Button type="submit" disabled={!isValid}>
           Create
         </Button>
