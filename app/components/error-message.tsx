7a50a625b3b9126f07e718fb69a16d9c909a5909
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-10 21:35:16 -0300

    Change ErrorMessage implementation

--- app/components/error-message.tsx
+++ app/components/error-message.tsx
@@ -1,26 +1,9 @@
 type ErrorMessageProps = {
-  error?: { _errors?: string[] };
-  onlyFirst?: boolean;
-  className?: string;
-};
+  message?: string;
+}
 
-/**
- * Display a list of errors (or a single one) form ZodFormattedErrors
- */
-export default function ErrorMessage({
-  error,
-  className = "",
-  onlyFirst = true,
-}: ErrorMessageProps) {
-  return error?._errors?.length ? (
-    <small className={"text-sm text-red-500 " + className}>
-      {onlyFirst
-        ? error._errors[0]
-        : error._errors.map((e, i, l) => (
-          <span key={e}>
-            {e} {i <= l.length ? <br /> : null}
-          </span>
-        ))}
-    </small>
-  ) : null;
+export default function ErrorMessage({ message }: ErrorMessageProps) {
+  if (!message) return null;
+
+  return <p><small className="text-red">{message}</small></p>;
 }
