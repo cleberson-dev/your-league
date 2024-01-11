f7a93a90c9b49ba2c1a5d347c4e2b0b942a13deb
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-10 21:40:38 -0300

    Refactor TeamCard

--- app/components/team-card.tsx
+++ app/components/team-card.tsx
@@ -6,15 +6,21 @@ type Props = {
   removable?: boolean;
 };
 
+const className = {
+  card: "relative flex h-36 w-40 select-none flex-col justify-end rounded-3xl border border-solid border-black/5 bg-primary py-4 text-center font-medium text-black/50 dark:bg-dark dark:text-white",
+  teamLogoContainer:
+    "absolute top-0 flex h-full w-full items-center justify-center",
+  teamLogo: "h-12",
+  teamName: "overflow-hidden text-ellipsis text-nowrap text-sm",
+};
+
 export default function TeamCard({ team }: Props) {
   return (
-    <div className="relative flex h-36 w-40 select-none flex-col justify-end rounded-3xl border border-solid border-black/5 bg-primary py-4 text-center font-medium text-black/50 dark:bg-dark dark:text-white">
-      <div className="absolute top-0 flex h-full w-full items-center justify-center">
-        <TeamLogo team={team} className="h-12" />
+    <div className={className.card}>
+      <div className={className.teamLogoContainer}>
+        <TeamLogo team={team} className={className.teamLogo} />
       </div>
-      <span className="overflow-hidden text-ellipsis text-nowrap text-sm">
-        {team.name}
-      </span>
+      <span className={className.teamName}>{team.name}</span>
     </div>
   );
 }
