09ce8595505d52a58eb72a22fb45acf3ed284660
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-11 23:36:01 -0300

    Create fixtures, leagues, tables, teams page

---
+++ app/routes/fixtures.tsx
@@ -0,0 +1,7 @@
+export default function FixturesPage() {
+  return (
+    <div>
+      <h1>Fixtures</h1>
+    </div>
+  );
+}
---
+++ app/routes/leagues.tsx
@@ -0,0 +1,7 @@
+export default function LeaguesPage() {
+  return (
+    <div>
+      <h1>Leagues</h1>
+    </div>
+  );
+}
