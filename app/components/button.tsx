9499b242514cd55e526a1fe09f08bdd024274c96
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-14 23:57:33 -0300

    Implement Button for 'sm' screens

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -61,7 +61,7 @@ export default function Button({
     <button
       {...props}
       className={cls(
-        "rounded px-4 py-2 text-sm transition-opacity hover:opacity-50 disabled:opacity-25",
+        "rounded px-3 py-2 lg:px-4 lg:py-2 text-xs lg:text-sm transition-opacity hover:opacity-50 disabled:opacity-25",
         className,
         getClassNameByVariant(variant)
       )}
