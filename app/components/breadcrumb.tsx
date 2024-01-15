b12f8d1e555fb0fc0f4a464a97f9f712586e0ecb
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-15 11:19:03 -0300

    Make Breadcrumb responsive

--- app/components/breadcrumb.tsx
+++ app/components/breadcrumb.tsx
@@ -6,7 +6,7 @@ type BreadcrumbProps = {
 
 export default function Breadcrumb({ items }: BreadcrumbProps) {
   return (
-    <p className="text-sm mb-3 flex gap-x-2 select-none">
+    <p className="text-xs lg:text-sm mb-1 lg:mb-3 flex gap-x-2 select-none">
       {items.map((item) => (
         <span key={item.label} className="after:inline-block after:ml-2 after:content-['/'] last:after:content-none last:opacity-50">
           {item.href ? <Link to={item.href} className="hover:text-violet">{item.label}</Link> : item.label}
