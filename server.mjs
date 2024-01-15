e516d09fada42ae2fdb22220079ef00b301241cc
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-14 21:21:44 -0300

    Add logger to production server

--- server.mjs
+++ server.mjs
@@ -1,5 +1,7 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
+import winston from "winston";
+import expressWinston from "express-winston";
 
 import * as build from "./build/index.js";
 
@@ -7,9 +9,21 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static("public"));
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+  ),
+  meta: true,
+  expressFormat: true,
+  colorize: false,
+}));
 
 app.all("*", createRequestHandler({ build }));
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
+  console.log(`Server running on port http://localhost:${PORT}/...`);
 });
